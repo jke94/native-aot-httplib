18864ecd66c5547a78f6cfc01787dfa263500636
jke94 <jcarracedo.dev@gmail.com>
2024-06-23 16:20:04 +0200

    Get httpStatusCode.

--- MyHttpLib/NativeExports.cs
+++ MyHttpLib/NativeExports.cs
@@ -4,34 +4,43 @@
     using System.Net.Http;
     using System.Runtime.InteropServices;
     using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
 
     public static class NativeExports
     {
-        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly IServiceCollection services = new ServiceCollection()
+            .AddHttpClient();
+
+        private static readonly ServiceProvider serviceProvider = services.BuildServiceProvider();
 
         [UnmanagedCallersOnly(EntryPoint = "MakeHttpGetRequest")]
-        public static unsafe int MakeHttpGetRequest(IntPtr urlPtr, IntPtr* resultPtr, int* resultSize)
+        public static unsafe int MakeHttpGetRequest(IntPtr urlPtr, IntPtr* resultPtr, int* resultSize, int* ptrHttpStatusCode)
         {
+            using var client = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
+
             try
             {
                 string url = Marshal.PtrToStringAnsi(urlPtr);
-                Task<string> task = httpClient.GetStringAsync(url);
-                task.Wait();
+                var task = Task.Run(() => client.GetAsync(url)).Result;
+                var httpStatusCode = (int)task.StatusCode;
+                var result = Task.Run(task.Content.ReadAsStringAsync).Result;
 
-                string result = task.Result;
                 byte[] resultBytes = System.Text.Encoding.UTF8.GetBytes(result);
                 *resultSize = resultBytes.Length;
                 *resultPtr = Marshal.AllocHGlobal(*resultSize);
+
+                *ptrHttpStatusCode = httpStatusCode;
+
                 Marshal.Copy(resultBytes, 0, *resultPtr, *resultSize);
 
-                return 0; // Success
+                return 0;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
                 *resultPtr = IntPtr.Zero;
                 *resultSize = 0;
-                return -1; // Failure
+                return -1;
             }
         }
 
--- main.cpp
+++ main.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
 #include <string>
-#include <cstring>
 
 // Declaración de las funciones exportadas de la librería compartida
-extern "C" int MakeHttpGetRequest(const char* url, void** resultPtr, int* resultSize);
+extern "C" int MakeHttpGetRequest(const char* url, void** resultPtr, int* resultSize, int* httpStatusCode);
 extern "C" void FreeMemory(void* ptr);
 
 int main() 
 {
-    const char* url = "https://jsonplaceholder.typicode.com/users";
+    const char* url = "https://jsonplaceholder.typicode.com/comments";
+
     void* resultPtr = nullptr;
     int resultSize = 0;
+    int httpStatusCode = 0;
 
-    int status = MakeHttpGetRequest(url, &resultPtr, &resultSize);
+    int status = MakeHttpGetRequest(url, &resultPtr, &resultSize, &httpStatusCode);
 
-    if (status == 0) 
+    if (status != 0)
     {
-        std::string result(static_cast<char*>(resultPtr), resultSize);
+        std::cerr << "Failed to make HTTP request" << std::endl;
 
-        std::cout << "HTTP Response: " << result << std::endl;
-        std::cout << "HTTP Result size: " << resultSize << std::endl;
-
-        FreeMemory(resultPtr);
-    } 
-    else 
-    {
-        std::cerr << "Failed to make HTTP request\n";
+        return -1;
     }
+    
+    std::string result(static_cast<char*>(resultPtr), resultSize);
+
+    std::cout << "HTTP Response: " << result << std::endl;
+    std::cout << "HTTP Result size: " << resultSize << std::endl;
+    std::cout << "HTTP Status code: " << httpStatusCode << std::endl;
+
+    FreeMemory(resultPtr);
 
     return 0;
 }
-
-
-
-/*
-g++ -o main main.cpp /home/javi/Repositories/native-aot-httplib/MyHttpLib/bin/Release/net8.0/linux-x64/publish/MyHttpLib.so -Wl,-rpath,/home/javi/Repositories/native-aot-httplib/MyHttpLib/bin/Release/net8.0/linux-x64/publish/
-*/
